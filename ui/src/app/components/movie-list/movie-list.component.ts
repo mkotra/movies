fdb9b49771f0b7b670779bdd3fb81da7a08256a2
mkotra <kotra.michal@gmail.com>
2025-02-20 21:22:58 +0100

    dummy ui to display movies

--- ui/src/app/components/movie-list/movie-list.component.ts
+++ ui/src/app/components/movie-list/movie-list.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, inject, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MovieService, Movie} from '../../services/movie.service';
 import {MatTableModule} from '@angular/material/table';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogModule} from '@angular/material/dialog';
 
 @Component({
   selector: 'movie-list',
@@ -15,6 +16,7 @@ export class MovieListComponent implements OnInit {
   currentPage: number = 1;
   totalPages: number = 0;
   pageSize: number = 10;
+  dialog = inject(MatDialog);
 
   constructor(private movieService: MovieService) {
   }
@@ -24,7 +26,7 @@ export class MovieListComponent implements OnInit {
   }
 
   loadMovies(): void {
-    this.movieService.getMovies(this.currentPage - 1 , this.pageSize).subscribe((response) => {
+    this.movieService.getMovies(this.currentPage - 1, this.pageSize).subscribe((response) => {
       this.movies = response.body ? response.body : [];
 
       const totalSizeHeader = response.headers.get('X-Total-Size');
@@ -36,6 +38,16 @@ export class MovieListComponent implements OnInit {
       console.log("Total: " + this.totalItems);
       console.log("Current Page: " + this.currentPage);
       console.log("Page Size: " + this.pageSize);
+    });
+  }
+
+  loadMovie(id: number) {
+    this.movieService.getMovie(id).subscribe((response) => {
+
+      this.dialog.open(DialogContent, {
+        data: response.body,
+        width: '400px',
+      });
     });
   }
 
@@ -52,5 +64,23 @@ export class MovieListComponent implements OnInit {
       this.loadMovies();
     }
   }
+}
+
+@Component({
+  imports: [MatDialogModule],
+  selector: 'dialog-content',
+  template: `
+    <h2 mat-dialog-title>{{ data.title }}</h2>
+    <mat-dialog-content>
+      <p><strong>Year:</strong> {{ data.year }}</p>
+      <p><strong>ID:</strong> {{ data.id }}</p>
+    </mat-dialog-content>
+    <mat-dialog-actions>
+      <button mat-button mat-dialog-close>Close</button>
+    </mat-dialog-actions>
+  `,
+})
+export class DialogContent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Movie) {}
 }
 
--- ui/src/app/services/movie.service.ts
+++ ui/src/app/services/movie.service.ts
@@ -22,23 +22,30 @@ export class MovieService {
   }
 
   getMovies(page: number, size: number): Observable<HttpResponse<Movie[]>> {
-    // Base64 encode the username and password for Basic Auth
-    const authorizationHeader = 'Basic ' + btoa(`${this.username}:${this.password}`);
+    const headers = this.authHeaders();
 
-    // Log the Authorization header for debugging purposes
-    console.log('Authorization Header:', authorizationHeader);
-
-    // Create HttpHeaders object with Authorization header
-    const headers = new HttpHeaders().set('Authorization', authorizationHeader);
-
-    // Send the GET request with the Authorization header and pagination params
     return this.http.get<Movie[]>(`${this.apiUrl}?page=${page}&page_size=${size}`, {headers, observe: 'response'}).pipe(
       catchError(error => {
-        // Log error for debugging
         console.error('Error occurred while fetching movies:', error);
         return throwError(() => new Error('Failed to fetch movies'));
       })
     );
+  }
+
+  getMovie(id: number): Observable<HttpResponse<Movie>> {
+    const headers = this.authHeaders();
+
+    return this.http.get<Movie>(`${this.apiUrl}/${id}`, {headers, observe: 'response'}).pipe(
+      catchError(error => {
+        console.error('Error occurred while fetching movie details:', error);
+        return throwError(() => new Error('Failed to fetch movie details'));
+      })
+    );
+  }
+
+  authHeaders(): HttpHeaders {
+    const authorizationHeader = 'Basic ' + btoa(`${this.username}:${this.password}`);
+    return  new HttpHeaders().set('Authorization', authorizationHeader);
   }
 }
 
