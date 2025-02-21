362761266a248cb4c1e8ece31ff4dab7fd4b4030
mkotra <kotra.michal@gmail.com>
2025-02-22 00:39:14 +0100

    improve repository

--- src/main/java/pl/mkotra/movies/storage/ActorRepository.java
+++ src/main/java/pl/mkotra/movies/storage/ActorRepository.java
@@ -1,5 +1,6 @@
 package pl.mkotra.movies.storage;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,14 @@ import pl.mkotra.movies.storage.entities.ActorDB;
 
 @Repository
 public interface ActorRepository extends JpaRepository<ActorDB, Integer> {
+
+    default Page<ActorDB> findByName(String title, Pageable pageable) {
+        if (StringUtils.isBlank(title)) {
+            return findAll(pageable);
+        } else {
+            return findByNameContaining(title, pageable);
+        }
+    }
+
     Page<ActorDB> findByNameContaining(String title, Pageable pageable);
 }
