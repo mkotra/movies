0835235dd48c62ef54de9ebe44ae4e56eff8fb83
mkotra <kotra.michal@gmail.com>
2025-02-22 23:18:46 +0100

    added index

--- src/main/java/pl/mkotra/movies/file/AppearancesProcessor.java
+++ src/main/java/pl/mkotra/movies/file/AppearancesProcessor.java
@@ -6,15 +6,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppearancesProcessor extends FileProcessor {
 
+    private static final String DROP_INDEX = "ALTER TABLE appearances DROP INDEX IF EXISTS idx_appearances_actor_id;";
     private static final String SQL = "INSERT INTO appearances(movie_id, actor_id, character_name ) VALUES(?, ?, ?);";
+    private static final String RECREATE_INDEX = "CREATE INDEX idx_appearances_actor_id ON appearances(actor_id);";
 
     AppearancesProcessor(JdbcTemplate jdbcTemplate) {
         super(jdbcTemplate);
     }
 
     @Override
+    protected void preProcess() {
+        jdbcTemplate.execute(DROP_INDEX);
+    }
+
+    @Override
     protected String getSql() {
         return SQL;
+    }
+
+    @Override
+    protected void postProcess() {
+        jdbcTemplate.execute(DROP_INDEX);
+        jdbcTemplate.execute(RECREATE_INDEX);
     }
 
     @Override
