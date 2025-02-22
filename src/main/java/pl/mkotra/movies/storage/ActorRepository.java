faa6651cb2186178234f622af0b444f1addf3da7
mkotra <kotra.michal@gmail.com>
2025-02-22 07:49:39 +0100

    improve repository

--- src/main/java/pl/mkotra/movies/storage/ActorRepository.java
+++ src/main/java/pl/mkotra/movies/storage/ActorRepository.java
@@ -1,6 +1,5 @@
 package pl.mkotra.movies.storage;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,14 +8,6 @@ import pl.mkotra.movies.storage.entities.ActorDB;
 
 @Repository
 public interface ActorRepository extends JpaRepository<ActorDB, Integer> {
-
-    default Page<ActorDB> findByName(String name, Pageable pageable) {
-        if (StringUtils.isBlank(name)) {
-            return findAll(pageable);
-        } else {
-            return findByNameContaining(name, pageable);
-        }
-    }
 
     Page<ActorDB> findByNameContaining(String name, Pageable pageable);
 }
--- src/main/java/pl/mkotra/movies/storage/MovieRepository.java
+++ src/main/java/pl/mkotra/movies/storage/MovieRepository.java
@@ -1,6 +1,5 @@
 package pl.mkotra.movies.storage;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,14 +8,6 @@ import pl.mkotra.movies.storage.entities.MovieDB;
 
 @Repository
 public interface MovieRepository extends JpaRepository<MovieDB, Integer> {
-
-    default Page<MovieDB> findByTitle(String title, Pageable pageable) {
-        if (StringUtils.isBlank(title)) {
-            return findAll(pageable);
-        } else {
-            return findByTitleContaining(title, pageable);
-        }
-    }
 
     Page<MovieDB> findByTitleContaining(String title, Pageable pageable);
 }
