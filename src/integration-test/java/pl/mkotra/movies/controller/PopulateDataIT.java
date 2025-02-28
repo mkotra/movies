e793b6112938c4ddc808ea5f2f9d64ce6f5d6bcf
mkotra <kotra.michal@gmail.com>
2025-02-28 16:36:07 +0100

    small fix

--- src/integration-test/java/pl/mkotra/movies/controller/PopulateDataIT.java
+++ src/integration-test/java/pl/mkotra/movies/controller/PopulateDataIT.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class PopulateDataIT extends BaseIT {
+class PopulateDataIT extends BaseIT {
 
     @Autowired
     private MovieRepository movieRepository;
--- src/integration-test/java/pl/mkotra/movies/controller/RateLimiterIT.java
+++ src/integration-test/java/pl/mkotra/movies/controller/RateLimiterIT.java
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class RateLimiterIT extends BaseIT {
+class RateLimiterIT extends BaseIT {
 
     @MockitoBean
     RateLimiterProperties rateLimiterProperties;
